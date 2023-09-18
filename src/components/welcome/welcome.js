5f9ce79a81d5574bce967b15907b2df4c4ffa12f
temfa <akinfetemitope5@gmail.com>
2023-09-18 06:59:27 +0100

    Done

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -1,27 +1,42 @@
 import React from "react";
 import "./welcome.css";
 import Header from "../header/header";
-import Layout from "../../utils/layout/layout";
 import PriButton from "../primary-button/priButton";
+import SecButton from "../sec-button/secButton";
+import Brands from "../brands/brands";
+import WelcomeImage from "../../assets/welcome.png";
+import Layout from "../../utils/layout/layout";
 
 const Welcome = () => {
   return (
     <div className="welcome-container">
       <Header />
-      <Layout>
-        <div className="welcome-hero">
-          <div className="welcome-text">
-            <h2>Banking made just for you</h2>
-            <p>Seamless Banking at your Fingertrips</p>
-          </div>
-          <div className="welcome-action">
-            <div>
-              <PriButton text="Get started" />
+      <div className="welcome-wrapper">
+        <Layout>
+          <div className="welcome-cont">
+            <div className="welcome-hero">
+              <div className="welcome-text">
+                <h2>
+                  Banking made just for <span>you</span>
+                </h2>
+                <p>Seamless Banking at your Fingertrips</p>
+              </div>
+              <div className="welcome-action">
+                <div>
+                  <PriButton text="Get started" />
+                </div>
+                <div>
+                  <SecButton text="Download App" />
+                </div>
+              </div>
             </div>
-            <div></div>
+            <div className="welcome-img">
+              <img src={WelcomeImage} alt="welcome" />
+            </div>
           </div>
-        </div>
-      </Layout>
+        </Layout>
+      </div>
+      <Brands />
     </div>
   );
 };
